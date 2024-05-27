5ff5e258933e109a150cf172e667e21f802682e3
willm-22 <williammoore2203@gmail.com>
2024-05-27 15:36:23 +0100

    no show mode

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -10,3 +10,4 @@ vim.cmd.colorscheme 'catppuccin-macchiato'
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.o.scrolloff = 8
+vim.o.showmode = false
